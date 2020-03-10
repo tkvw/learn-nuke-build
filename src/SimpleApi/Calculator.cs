304fb9b54755e67e7851ed0ae184d6c03b6bccd5
Dennie de Lange <dennie@tkvw.nl>
2020-03-10 10:35:45 +0100

    Finished build library project SimpleApi

---
+++ src/SimpleApi/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace SimpleApi
+{
+    public class Calculator
+    {
+        public int Add(params int[] numbers)
+        {
+            return numbers.Aggregate(0, (sum, number) => sum + number);
+        }
+        public int Subtract(params int[] numbers)
+        {
+            return numbers.Aggregate(0, (sum, number) => sum + number);
+        }
+    }
+}
